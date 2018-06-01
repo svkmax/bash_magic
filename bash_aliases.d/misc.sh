e40a6de827d30f992dbdbcc8f31b866fb734c2bf
joe fleming <joe.fleming@gmail.com>
2018-06-01 14:48:42 -0700

    feat: add paste function
    useful for piping text or other output to hastebin

--- bash_aliases.d/misc.sh
+++ bash_aliases.d/misc.sh
@@ -6,6 +6,13 @@ decrypt () {
 	openssl aes-256-cbc -d -a -in "${1}" -out "${1%.openssl}"
 }
 
+paste () {
+	read -d '' IN
+	curl --silent -H "Content-Type: application/text" -X POST -d "$IN" https://hastebin.com/documents \
+		| sed 's/^{\"key\":\"\(.*\)\"}$/https\:\/\/hastebin.com\/raw\/\1/'
+	echo
+}
+
 alias ..='cd ..'
 alias ...='cd ..; cd ..'
 alias yis='yarn install && yarn start'
